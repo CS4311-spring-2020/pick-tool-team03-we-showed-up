53b472c1f16cc928d1be8eaaf4a3be40529f110c
Matthew Iglesias <miglesias4@miners.utep.edu>
2020-04-01 14:07:11 -0600

    PyTesseract Phase 2: Procedure and define directories

--- src/Transcribers/OCRFeeder.py
+++ src/Transcribers/OCRFeeder.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pytesseract as pt
 import argparse
+import difflib
 import os
 
 from IngestionFunctionality import new_path
@@ -8,15 +9,25 @@ from IngestionFunctionality import new_path
 #PyTesseract uses Pillow to import the necessary packages needed for transcribing image files. The Image class in PIL
 # loads the input from disk in PIL format, required for PyTesseract.
 
-f_parser = argparse.ArgumentParser() # Initialize setup for parsing imported file
-f_parser.add_argument("-i", "--image", required=True, help="new_path")
-f_args = vars(f_parser.parse_args())
+#f_parser = argparse.ArgumentParser() # Initialize setup for parsing imported file
+#f_parser.add_argument("-i", "--image", required=True, help="new_path")
+#f_args = vars(f_parser.parse_args())
 
 path = new_path # Source Folder/Directory of raw files
-
 final_path = new_path # Destination Folder/Directory of transcribed files
-for img in os.listdir(path):
-    inputPath = os.path.join(path,img)
 
-    image_file = Image.open(inputPath)
-    image_text = pt.image_to_string(image_file, lang ="eng")
+def main():
+    for img in os.listdir(path):
+        inputPath = os.path.join(path,img)
+
+        image_file = Image.open(inputPath) #Opens file, pre-transcription
+        image_text = pt.image_to_string(image_file, lang ="eng") #Transcribes the image file
+
+        f_path = os.path.join(final_path,'time_'+ img + ".txt") # Set path to store in specified final_path
+        f_parsed = open(f_path,"w") # Write to specified path
+        f_parsed = write(image_text)
+        f_parsed.close() # f_parsed is the new transcrbed file
+
+
+if __name__ == "__main__":
+    pass
