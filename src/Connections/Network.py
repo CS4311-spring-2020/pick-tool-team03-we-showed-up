f602bc23fa4508e8888198316b4b778c2b5cd43d
gummo <jess.redekop@gmail.com>
2020-04-14 12:07:19 -0600

    Network connection works

--- src/Connections/Network.py
+++ src/Connections/Network.py
@@ -1,45 +1,62 @@
 # This Python file uses the following encoding: utf-8
+import threading
 import socket
 
 class Network:
     def __init__(self):
         # Ask clients what port they would like to connect through
-        port = 12345
+        self.port = 8091
         #local port for now
-        lead_ip = '127.0.0.1'
+        #lead_ip = '127.0.0.1'
+        # Lead socket
+        self.socket = socket.socket()
+
+        self.serverStatus = False
+
         pass
 
     def start_lead_server(self):
         s = socket.socket()
         print ("Socket successfully created")
-        s.bind(('', port))
-        print ("socket binded to %s" %(port))
+        s.bind(('', self.port))
+        print ("socket binded to %s" %(self.port))
+
+        self.socket = s
+        self.serverStatus == True
 
         #20 connections will be accepted per SRS
-        s.listen(20)
+        self.socket.listen(20)
         print ("socket is listening")
+        thread = threading.Thread(target=self.start_server_thread)
+        thread.start()
 
-        while True:
+    def start_server_thread(self):
+        while (self.serverStatus == True):
+            print('Hi')
+            # Establish connection with client.
+            c, addr = self.socket.accept()
+            print ('Got connection from', addr)
 
-           # Establish connection with client.
-           c, addr = s.accept()
-           print ('Got connection from', addr)
+            # send a thank you message to the client.
+            c.send('Thank you for connecting')
 
-           # send a thank you message to the client.
-           c.send('Thank you for connecting')
+            # Close the connection with the client
+            # c.close()
 
-           # Close the connection with the client
-           c.close()
+    def close_server(self):
+        self.socket.close()
+        self.serverStatus = False
+        self.socket = socket.socket()
 
-    def connect_analyst_to_lead(self):
+    def connect_analyst_to_lead(self, lead_ip):
         s = socket.socket()
 
         # Define the port on which you want to connect
 
         # connect to the server on local computer
-        s.connect((lead_ip, port))
+        s.connect((lead_ip, self.port))
 
         # receive data from the server
         print (s.recv(1024))
         # close the connection
-        s.close()
+        # s.close()
